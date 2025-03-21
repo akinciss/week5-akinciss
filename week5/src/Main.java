b4c488f52d4d3e9a604637e840c8602d515348ab
Simge Akıncı <148821014+akinciss@users.noreply.github.com>
2025-03-21 12:39:26 +0300

    Update Main.java

--- week5/src/Main.java
+++ week5/src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Point p1 = new Point();
+        System.out.println(p1.xCoord);
+        System.out.println(p1.yCoord);
+
+        Point p2 = new Point(3, 4);
+        System.out.println(p2.xCoord);
+        System.out.println(p2.yCoord);
+
+        Point p3 = new Point(5);
+        System.out.println(p3.xCoord);
+        System.out.println(p3.yCoord);
+
+        Rectangle r1 = new Rectangle(new Point(3, 4), 5, 6);
+
+        System.out.println(r1.getSideA());
+        System.out.println(r1.getSideB());
+        System.out.println(r1.getTopleft().xCoord);
+        System.out.println(r1.getTopleft().yCoord);
+        System.out.println(r1.area());
+        System.out.println(r1.perimeter());
     }
 }
